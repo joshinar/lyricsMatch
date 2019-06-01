eaa871f1cfbdd3c49aed37726b8b94e4d45aa92c
naren <joshinaren93@gmail.com>
2019-06-01 14:19:07 +0530

    Updates

---
+++ precache-manifest.e5afba38ccea6e5bc00da176c8a94819.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f9120af15676da501062d25036610a",
+    "url": "/lyricsMatch/index.html"
+  },
+  {
+    "revision": "09b9e06769fa4ab257bb",
+    "url": "/lyricsMatch/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "8cbcd2e3d1245c93c65d",
+    "url": "/lyricsMatch/static/js/2.9ea2ce6f.chunk.js"
+  },
+  {
+    "revision": "09b9e06769fa4ab257bb",
+    "url": "/lyricsMatch/static/js/main.0c689bb8.chunk.js"
+  },
+  {
+    "revision": "7687140290eb0aff871c",
+    "url": "/lyricsMatch/static/js/runtime~main.d38bcabb.js"
+  }
+]);
